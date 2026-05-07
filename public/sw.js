31d98da3681f6438c746f3d4e5342dbdb687d39e
iDaedalus <jerometecsonn@gmail.com>
2026-05-07 12:09:26 +0800

    xcxc

--- public/sw.js
+++ public/sw.js
@@ -92,7 +92,7 @@
 //   event.notification.close();
 //   event.waitUntil(clients.openWindow("/"));
 // });
-const CACHE_NAME = "zxc-stream-v2.88";
+const CACHE_NAME = "zxc-stream-v2.90";
 const urlsToCache = [
   "/",
   "/manifest.json",
